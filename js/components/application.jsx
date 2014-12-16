f9247ae0704d9dc86aedc695f3089493b9bf34de
Yomguithereal <guillaumeplique@gmail.com>
2014-12-16 17:11:00 +0100

    @danieleguido layout

--- js/components/application.jsx
+++ js/components/application.jsx
@@ -9,6 +9,44 @@ var React = require('react'),
 
 module.exports = React.createClass({
   render: function() {
-    return <RouteHandler />;
+    return (
+      <div id="layout">
+
+        // Header
+        <header>
+          <div className="inner">
+            <div id="logo">Agent Smith</div>
+            <div id="query">
+              <textarea rows="1" placeholder="neo4j query ..." ng-model="query"></textarea>
+            </div>
+          </div>
+        </header>
+
+        // Menu
+        <nav>
+          <div className="inner">
+            <a href="/" ><div>~</div></a>
+            <a href="/settings" className="active"><div>s</div></a>
+            <a href="/design"><div>d</div></a>
+            <a href="/agent-smith"><div>w</div></a>
+          </div>
+        </nav>
+
+        // Sidebar
+        <aside>
+          <div className="inner">
+            <RouteHandler />
+          </div>
+        </aside>
+
+        // Sigma playground
+        <div id="playground">
+          <div className="inner">
+            <div sigma id="ground" data="network"></div>
+          </div>
+        </div>
+
+      </div>
+    );
   }
 });
