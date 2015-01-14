9fbd7deba5a6c04466408d1b0aceaecd0b672ba1
Yomguithereal <guillaumeplique@gmail.com>
2015-01-14 16:50:10 +0100

    Query highlight proto

--- js/components/query.jsx
+++ js/components/query.jsx
@@ -5,39 +5,60 @@
  * Query textarea holding the current cypher query.
  */
 var React = require('react'),
+    ace = require('brace'),
     controller = require('../controller.js');
+
+// Loading ace assets
+require('../lib/mode-cypher.js');
+require('brace/theme/dawn');
 
 // TODO: cannot bind to app state so drastically
 module.exports = React.createClass({
   mixins: [controller.mixin],
-  cursor: ['query'],
-  handleChange: function(e) {
+  componentDidMount: function() {
+    var self = this;
 
-    // TODO: cannot commit asynchronously here
-    this.cursor.edit(e.target.value).commit();
+    // Loading ace editor
+    this.editor = ace.edit('query-editor');
+    this.editor.getSession().setMode('ace/mode/cypher');
+    this.editor.setTheme('ace/theme/dawn');
+
+    // Configuration
+    this.editor.setHighlightActiveLine(false)
+    this.editor.renderer.setShowGutter(false);
+
+    // Update editor along with the app's state
+    this.cursor = controller.select('query');
+    this.listener = function() {
+      self.editor.setValue(self.cursor.get());
+      self.editor.clearSelection();
+    };
+
+    this.listener();
+
+    this.cursor.on('update', this.listener);
   },
-  handleKeyPress: function(e) {
-    if (e.which !== 13)
-      return;
+  handleKeyDown: function(e) {
 
-    e.preventDefault();
-
-    controller.emit('query', e.target.value);
+    // Firing if ctrl+enter
+    if (e.ctrlKey && e.which === 13)
+      controller.emit('query', this.editor.getValue());
   },
   render: function() {
     return (
       <div className="grid">
         <div>
-          <textarea rows="1"
-                    spellCheck={false}
-                    onChange={this.handleChange}
-                    onKeyPress={this.handleKeyPress}
-                    value={this.cursor.get()}
-                    placeholder="Neo4J query ..." />
+          <div id="query-editor"
+               onKeyDown={this.handleKeyDown}
+               placeholder="Neo4J query ...">
+          </div>
         </div>
         <button ><i className="fa fa-save"></i></button>
         <button className="primary"><i className="fa fa-play"></i></button>
       </div>
     );
+  },
+  componentWillUnmount: function() {
+    this.cursor.off('update', this.listener);
   }
 });
