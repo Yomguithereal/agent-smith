4815f3f0b13d0d743d49b9876c567f738ffe3679
Daniele Guido <gui.daniele@gmail.com>
2015-01-15 18:45:48 +0100

    graph

--- js/components/statistics.jsx
+++ js/components/statistics.jsx
@@ -11,17 +11,21 @@ module.exports = React.createClass({
   mixins: [controller.mixin],
   cursor: ['graph'],
   render: function() {
-
     var graph = this.cursor.get();
 
     if (!graph)
-      return null;
+      return (
+        <div id="statistics">
+          <div className="action selected switch-view">graph</div><div className="action switch-view">data</div>
+        </div>
+      );
 
     return (
       <div id="statistics">
         <div className="inner">
-          <span className="number">{graph.nodes.length}</span> nodes, <span  className="number">{graph.edges.length}</span> edges
+          <span className="number">{graph.nodes.length}</span> nodes, <span  className="number">{graph.edges.length}</span> edges     
         </div>
+        <div className="action selected switch-view">graph</div><div className="action switch-view">graph</div>
       </div>
     );
   }
